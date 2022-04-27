aa3b2b95c3814aabbf49b6f4bdd8898821c6b096
Remy <remy12.salem@epitech.eu>
2022-04-27 17:44:00 +0200

    add firebase option

--- ubergrass/lib/main.dart
+++ ubergrass/lib/main.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:ubergrass/src/firebase/firebase.dart';
 import 'package:ubergrass/src/page/settings/settings_controller.dart';
 import 'package:ubergrass/src/page/settings/settings_service.dart';
-
+import 'firebase_options.dart';
 import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final settingsController = SettingsController(SettingsService());
   await settingsController.loadSettings();
-  FirebasePackage.init(await Firebase.initializeApp());
+  FirebasePackage.init(await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,));
   runApp(MyApp(settingsController: settingsController));
 }
